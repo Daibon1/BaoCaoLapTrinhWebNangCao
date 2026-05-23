e70338a152bdbe183d97d8839d94f0c1444ab9aa
Nguyen Van Dai <saovayem36@gmail.com>
2026-05-24 03:01:38 +0700

    cuoi 2

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -30,8 +30,8 @@ const nodemailer = require('nodemailer');
 module.exports.sendMail = (email, subject, html) => {
     const transporter = nodemailer.createTransport({
         host: 'smtp.resend.com',
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,
         auth: {
             user: 'resend',
             pass: process.env.RESEND_API_KEY
