3fab84d65ef0920140c722c72e69f06f9c17c9b9
Nguyen Van Dai <saovayem36@gmail.com>
2026-05-24 02:26:55 +0700

    test1

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,7 +1,32 @@
 // Source - https://stackoverflow.com/q/19877246
 // Posted by tonymx227, modified by community. See post 'Timeline' for change history
 // Retrieved 2026-05-08, License - CC BY-SA 3.0
+// const nodemailer = require('nodemailer');
+// module.exports.sendMail = (email, subject, html) => {
+//     const transporter = nodemailer.createTransport({
+//         service: 'gmail',
+//         auth: {
+//             user: process.env.EMAIL_USER,
+//             pass: process.env.EMAIL_PASSWORD
+//         }
+//     });
+//     const mailOptions = {
+//         from: process.env.EMAIL_USER,
+//         to: email,
+//         subject: subject,
+//         html: html
+//     };
+//     transporter.sendMail(mailOptions, function(error, info){
+//         if (error) {
+//             console.log(error);
+//         } else {
+//             console.log('Email sent: ' + info.response);
+//         }
+//     })
+
+// }
 const nodemailer = require('nodemailer');
+
 module.exports.sendMail = (email, subject, html) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -10,18 +35,24 @@ module.exports.sendMail = (email, subject, html) => {
             pass: process.env.EMAIL_PASSWORD
         }
     });
+
+    console.log("[sendMail] Gửi tới:", email);
+    console.log("[sendMail] EMAIL_USER:", process.env.EMAIL_USER);
+    console.log("[sendMail] EMAIL_PASSWORD set:", !!process.env.EMAIL_PASSWORD);
+
     const mailOptions = {
         from: process.env.EMAIL_USER,
         to: email,
         subject: subject,
         html: html
     };
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    })
 
-}
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.error("[sendMail] Lỗi gửi mail:", error.message);
+            console.error("[sendMail] Chi tiết:", error);
+        } else {
+            console.log("[sendMail] Gửi thành công:", info.response);
+        }
+    });
+};
