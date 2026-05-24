80887b97283775e036d07c5f4f681cbd26f3778d
Nguyen Van Dai <saovayem36@gmail.com>
2026-05-24 22:28:03 +0700

    deploy raiway2

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,30 +1,30 @@
 // Source - https://stackoverflow.com/q/19877246
 // Posted by tonymx227, modified by community. See post 'Timeline' for change history
 // Retrieved 2026-05-08, License - CC BY-SA 3.0
-const nodemailer = require('nodemailer');
-module.exports.sendMail = (email, subject, html) => {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
-        }
-    });
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to: email,
-        subject: subject,
-        html: html
-    };
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    })
+// const nodemailer = require('nodemailer');
+// module.exports.sendMail = (email, subject, html) => {
+//     const transporter = nodemailer.createTransport({
+//         service: 'gmail',
+//         auth: {
+//             user: process.env.EMAIL_USER,
+//             pass: process.env.EMAIL_PASSWORD
+//         }
+//     });
+//     const mailOptions = {
+//         from: process.env.EMAIL_USER,
+//         to: email,
+//         subject: subject,
+//         html: html
+//     };
+//     transporter.sendMail(mailOptions, function(error, info){
+//         if (error) {
+//             console.log(error);
+//         } else {
+//             console.log('Email sent: ' + info.response);
+//         }
+//     })
 
-}
+// }
 // const nodemailer = require('nodemailer');
 
 // module.exports.sendMail = async (email, subject, html) => {
@@ -86,3 +86,32 @@ module.exports.sendMail = (email, subject, html) => {
 //         return false;
 //     }
 // };
+const nodemailer = require('nodemailer');
+
+module.exports.sendMail = (email, subject, html) => {
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        family: 4,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    });
+
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: subject,
+        html: html
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.error("[sendMail] Lỗi:", error.message);
+        } else {
+            console.log("[sendMail] Thành công:", info.response);
+        }
+    });
+};
