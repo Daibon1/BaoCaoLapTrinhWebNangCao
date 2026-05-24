2ccc200fc67e793997f3e38e581ff1cc7e3da2b1
Nguyen Van Dai <saovayem36@gmail.com>
2026-05-24 10:52:44 +0700

    BaoCao

--- api/admin/index.route.js
+++
@@ -1,9 +0,0 @@
-const jobRouter = require("./job.route");
-const authRouter = require("./auth.route");
-const accountRouter = require("./account.route");
-const systemConfig = require("../../config/system");
-
-module.exports = (app) => {
-    app.use(`${systemConfig.prefixAdmin}/api/job`, jobRouter);
-    app.use(`${systemConfig.prefixAdmin}/api/auth`, authRouter);
-}
